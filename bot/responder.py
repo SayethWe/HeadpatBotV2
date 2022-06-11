990832a4261076df26f7fb13df29247adc6ef6f1
geekman9097 <powerplanets@aol.com>
2022-06-11 12:15:22 -0600

    responder logging fix, newline capability

--- bot/responder.py
+++ bot/responder.py
@@ -1,16 +1,15 @@
-import os
+import logging, os
 import yaml
 from numpy.random import default_rng
 from glom import glom
-import logging
 
-logger = logging.getLogger('discord')
+logger = logging.getLogger(os.environ['LOGGER_NAME'])
 
 with open(os.path.join("bot","responses.yaml"),"r") as file:
     try:
         responses = yaml.safe_load(file)
     except yaml.YAMLError as err:
-        logger.debug(err)
+        logger.error(err)
 
 rng = default_rng()
 
@@ -18,4 +17,4 @@ def getResponse(request:str, *args : str) -> str:
     request=request.upper()
     replies = glom(responses,request)
     reply = rng.choice(replies)
-    return reply.format(*args)
+    return reply.replace('[NEWLINE]','\n').format(*args)
